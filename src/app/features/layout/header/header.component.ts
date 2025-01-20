06fc00746498e1cd909176fc178c72d4c04fdaad
vivekks1293 <vivekks1293@gmail.com>
2025-01-20 23:12:58 +0530

    Logo added

--- src/app/features/layout/header/header.component.ts
+++ src/app/features/layout/header/header.component.ts
@@ -3,13 +3,18 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: false,
-  
+
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
-  navigateToDashboard(){
+  constructor(private router: Router) { }
+  isClient = false;
+
+  ngOnInit() {
+    this.isClient = true; // Ensures rendering only on the client
+  }
+  navigateToDashboard() {
     this.router.navigate(['/']);
   }
 }
